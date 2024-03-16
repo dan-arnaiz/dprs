9cd03f3262a6f0c577b5f1a37897bb6abae40391
dan-arnaiz <dfarnaiz@gmail.com>
2024-03-16 10:24:06 +0800

    Closed Workspace

---
+++ src/main/java/com/mycompany/dentalpatientrecordsystem/DentalPatientRecordSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dentalpatientrecordsystem;
+
+/**
+ *
+ * @author Marki Querequincia
+ */
+
+
+public class DentalPatientRecordSystem {
+    
+    public static void main(String[] args) {
+        System.out.println("It ran!");
+        loginPopUp login = new loginPopUp();
+        login.displayForm();
+    }
+}
